759052a8d34cea9edeef5699e656ee0d02996858
Aruminiya <leo80103@gmail.com>
2024-10-30 12:17:35 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+import BonTalk from "@bonvies/bontalk";
+// const [open, setOpen] = useState(false);
+
 function App() {
+  const [bonTalk, setBonTalk] = useState<BonTalk | null>(null);
+
+  const handleOpenPanel = () => {
+    if (bonTalk) {
+      bonTalk.togglePanel();
+      return
+    }
+    // create an usable sip phone instance
+    const bonTalkInstance = new BonTalk({
+      wsServer: "wss://bonuc.sbc.telesale.org:7443/ws",
+      domains: ["bonuc.sbc.telesale.org"],
+      username: "3004",
+      password: "1234",
+      displayName: "Leo",
+    });
+    bonTalkInstance.init();
+    setBonTalk(bonTalkInstance);
+  };
 
   return (
     <>
       <h1>Bontalk app test</h1>
-      <button>Click me</button>
+      <button
+        onClick={handleOpenPanel}
+      >
+        Start BonTalk App
+      </button>
     </>
   )
 }
